d635d4bf9708a95b24d9ca8311fc9c17315f536a
pal-oe <114949236+pal-oe@users.noreply.github.com>
2023-09-26 15:53:09 +0300

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Replace 'YOUR_BOT_TOKEN' with the token obtained from BotFather
-const bot = new TelegramBot('6179436473:AAHrPLLZBSjks3kzs5rvEVcD52UaAellVzE', { polling: true });
+const bot = new TelegramBot('6179436473:AAHrPLLZBSjks3kzs5rvEVcD52UaAellVzE');
 
 // Array of chat IDs
 const chatIds = ['856048902', '495806103', '1001459438185']; // Add the desired chat IDs
@@ -15,4 +15,11 @@ bot.onText(/\/submit/, (msg) => {
   chatIds.forEach((chatId) => {
     bot.sendMessage(chatId, message);
   });
+});
+
+// Set up the webhook
+bot.setWebHook('YOUR_WEBHOOK_URL').then(() => {
+  console.log('Webhook is set up successfully');
+}).catch((error) => {
+  console.error('Error setting up webhook:', error);
 });
