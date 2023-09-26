768cea0925031de3b6e648f700f0ace03ea4624b
pal-oe <114949236+pal-oe@users.noreply.github.com>
2023-09-26 15:58:10 +0300

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,25 +1,22 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Replace 'YOUR_BOT_TOKEN' with the token obtained from BotFather
-const bot = new TelegramBot('6179436473:AAHrPLLZBSjks3kzs5rvEVcD52UaAellVzE');
+const bot = new TelegramBot('6179436473:AAHrPLLZBSjks3kzs5rvEVcD52UaAellVzE', { polling: true });
+
+//6024456106:AAGhVJoQvhiHaDhhsjle5VauHopwMlXNTLY
 
 // Array of chat IDs
 const chatIds = ['856048902', '495806103', '1001459438185']; // Add the desired chat IDs
 
-// Handle the /submit command
-bot.onText(/\/submit/, (msg) => {
-  const chatId = msg.chat.id;
-  const message = 'New order received: ' + msg.text.replace('/submit', '');
+// Handle incoming messages
+bot.on('message', (msg) => {
+  const message = msg.text;
+
+  // Process the message
+  // You can add your own logic here
 
   // Send a message to each chat ID
   chatIds.forEach((chatId) => {
-    bot.sendMessage(chatId, message);
+    bot.sendMessage(chatId, New order received: ${message});
   });
-});
-
-// Set up the webhook
-bot.setWebHook('YOUR_WEBHOOK_URL').then(() => {
-  console.log('Webhook is set up successfully');
-}).catch((error) => {
-  console.error('Error setting up webhook:', error);
 });
