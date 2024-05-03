78598150872bc01f92b097660ad7c7e9644888ea
pal-oe <fenanyosef@gmail.com>
2024-05-03 11:58:22 +0300

    done addicg section wrapper

---
+++ My portfolio/src/hoc/index.js
@@ -0,0 +1,3 @@
+import SectionWrapper from "./sectioWrapper";
+
+export { SectionWrapper }
--- My portfolio/src/hoc/sectioWrapper.jsx
+++ My portfolio/src/hoc/sectioWrapper.jsx
@@ -1,12 +1,24 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { motion } from 'framer-motion'
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
 
-const SectionWrapper = () => {
-    return (
-        <div>d</div>
-    )
-}
+const SectionWrapper = (Component, idName) =>
+    function HOC() {
+        return (
+            <motion.section
+                variants={staggerContainer()}
+                initial='hidden'
+                whileInView='show'
+                className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+            >
+                <span className="hash-span" id={idName}>
+                    &nbsp;
+                </span>
+                <Component />
+            </motion.section>
+        )
+    }
 
 export default SectionWrapper
